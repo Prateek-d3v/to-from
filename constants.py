c563140669d3dc7c3bef2c4f99e3f7afff1e2c16
Naga-d3v <naga@d3vtech.com>
2024-07-23 17:00:28 +0530

    get products code1

--- constants.py
+++ constants.py
@@ -17,7 +17,7 @@ GENERATION_CONFIG = {
 }
 SYSTEM_INSTRUCTIONS = """
 #CONTEXT#
-You are a helpful gifting assistant and you must always refer to the uploaded file before giving answers. The file a list of attributes matching the questions. Analyze the user query, try to match it with the information given in the file, and return the matching attributes.
+You are a helpful gifting assistant, and you must always refer to the uploaded file before giving answers. The file contains a list of attributes matching the questions. Analyze the user query, try to match it with the information given in the file, and return the matching attributes.
 
 
 #FILE_DESCRIPTION#
@@ -28,7 +28,7 @@ occasions.txt - This contains an array of occasions. Get occasions from this arr
 
 #INSTRUCTIONS#
 To complete the task, you need to follow these steps:
-Your task is to analyze the user query and return all the relevant attributes where the keywords from the query are matching either the attribute name, synonyms or description. Also return occasion, relation and price range.
+Your task is to analyze the user query and return all the relevant attributes where the keywords from the query match either the attribute name, synonyms or description. Also return occasion, relation, and price range.
  If no match is found, return "NA."
 ensuring all values are unique. For example, if 'T_Gardening' appears multiple times, only include it once.
 Based on your knowledge, you can infer additional attributes where appropriate.
@@ -48,6 +48,7 @@ Example -
 #FEW_SHOT_EXAMPLES#
 1. Example #1
 Input: "Can you please provide some options for a Father's Day gift? My husband spends a lot of time on a recliner. A high quality wearable blanket that doesn’t slip off. Not too long so he doesn’t trip over. Materials that’s all season. Budget: $100."
+Thoughts: The query indicates the occasion is Father's Day, the relation is husband, and the budget is $100. The gift should be related to comfort and practicality, specifically mentioning a wearable blanket. Relevant attributes include those related to home decor and comfort items like throws and blankets.
 Output: 
 {
 “attributes”: ["C_Home","SC_Decor","T_Throws &  Blankets"],
@@ -57,13 +58,25 @@ Output:
 }
 
 2. Example #2
+Input: " I need a gift for my BFF's birthday - she's a lawyer and very serious but loves travel and food, fashion and dining. She is very put together and does not cook - she prefers to go out vs staying in. Budget is $50."
+Thoughts: The query says her friend is a lawyer and she likes travelling, food, fashion and dining. So, you should include attributes like "Travel" "Food & Drink" "professional" "career oriented" "Fashion" "on-trend". The query also says that her friend doesn't like to cook and she prefer going out vs staying in. So, you should exclude attributes like "Kitchen" "entertaining" etc...
+Output: 
+{
+“attributes”: [“C_Lifestyle”, “SC_Travel”, “T_Travel Accessories”, “T_Luggage”, “SC_Jewelry”, “Jewelry Pref-Fashion Jewelry”, “Jewelry Pref-Dainty Jewelry”, “T_Earrings”, "T_Necklaces", "T_Bracelets", "T_Handbags_Sm", "T_Handbags_Md", "SC_Food & Drink", "T_Gift Boxes, Sets, & Collections", "T_Restaurant Gift Card"],
+“occasion”: [“Birthday”],
+“relation”: [“Friend”],
+“price_range”: ["$45-$55"]
+}
+
+3. Example #3
 Input: "Hi! My son’s 3rd birthday is coming up. Can you send over some gift options around $50? He loves fire trucks, puppies, Paw Patrol, trains, art, and music/dancing"
+Thoughts: The query indicates the occasion is a birthday, the relation is son (child), and the budget is $50. The son's interests include fire trucks, puppies, Paw Patrol, trains, art, and music/dancing. Relevant attributes should include items related to kids' outdoor games, imaginative play, transportation and vehicles, arts and music, and technology and electronics. The budget range is specified as around $50. 
 Output: 
 {
 “attributes”:[ "C_Kids", "SC_Kids' Outdoors", "T_Kids' Outdoor Games & Toys", "SC_Kids' Imaginative Play", "T_Kids' Transportation, Vehicles, & Trains", "T_Kids' Blocks & Building", "T_Kids' Play Structures", "SC_Kids' Arts & Music", "T_Kids' Music", "SC_Kids' Technology", "T_Kids' Tech & Electronics"],
 “occasion”: [“Birthday”],
 “relation”: ["Kids"],
-“price_range”: [$45-$55]
+“price_range”: ["$45-$55"]
 }
 
 #RECAP#
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import vertexai
 from vertexai.generative_models import GenerativeModel
 import constants as const
-import helper as helper
+import helper
 
 # Set the environment variable for Google Application Credentials
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = const.SA_ACCOUNT
@@ -24,11 +24,11 @@ occasions = helper.read_text_file(const.OCCASIONS_PATH)
 relations = helper.read_text_file(const.RELATIONS_PATH)
 
 # Load the attributes, occasions, and relations JSON files
-with open('files/sqlout-attribute.json', 'r') as f:
+with open('files/sqlout-attribute.json', 'r', encoding='utf-8') as f:
     attributes_data = json.load(f)
-with open('files/sqlout-occasion.json', 'r') as f:
+with open('files/sqlout-occasion.json', 'r', encoding='utf-8') as f:
     occasions_data = json.load(f)
-with open('files/sqlout-relationship.json', 'r') as f:
+with open('files/sqlout-relationship.json', 'r', encoding='utf-8') as f:
     relations_data = json.load(f)
 
 # Define the prompt template
@@ -51,24 +51,21 @@ def get_ids(names, data, key_name):
                 ids.append(item["id"])
     return ids
 
-# Input loop for querying the model
-query = input("Query: ")
-while query != 'q':
-    prompt = prompt_template.format(attributes, occasions, relations, query)
-    response = model.generate_content([prompt])
-    
-    # Check if the response is empty or not
-    if not response.text:
-        print("Error: Empty response from the model.")
-        query = input("User Query: ")
-        continue
-    
+# Input for querying the model
+query = "I need a gift for my BFF's birthday - she's a lawyer and very serious but loves travel and food, fashion and dining. She is very put together and does not cook - she prefers to go out vs staying in. Budget is $50."
+prompt = prompt_template.format(attributes, occasions, relations, query)
+response = model.generate_content([prompt])
+
+# Check if the response is empty or not
+if not response.text:
+    print("Error: Empty response from the model.")
+else:
     response_text = response.text.replace('“', '"').replace('”', '"').replace('```', '').replace('json', '').strip()
 
     try:
         # Parse the JSON response
         response_data = json.loads(response_text)
-    
+
         # Ensure price_range is properly formatted
         if 'price_range' in response_data:
             price_range = response_data['price_range']
@@ -79,51 +76,51 @@ while query != 'q':
     except json.JSONDecodeError as e:
         print("Error decoding JSON:", str(e))
         print("Response text:", response_text)
-        query = input("User Query: ")
-        continue
-    
+        response_data = {}
+
     # Extract IDs for attributes, occasions, and relations
-    attribute_ids = get_ids(response_data["attributes"], attributes_data, "name")
-    occasion_ids = get_ids(response_data["occasion"], occasions_data, "name")
-    relation_ids = get_ids(response_data["relation"], relations_data, "name")
-    
+    attribute_ids = get_ids(response_data.get("attributes", []), attributes_data, "name")
+    occasion_ids = get_ids(response_data.get("occasion", []), occasions_data, "name")
+    relation_ids = get_ids(response_data.get("relation", []), relations_data, "name")
+
     # Get the price range
     price_range = response_data.get("price_range", [])
-    min_price = price_range[0] if price_range else ""
-    max_price = price_range[1] if len(price_range) > 1 else ""
+    min_price = int(price_range[0] * 100) if len(price_range) > 0 else ""
+    max_price = int(price_range[1] * 100) if len(price_range) > 1 else ""
 
-    print("attributes", attribute_ids)
-    print("occasions", occasion_ids)
-    print("relations", relation_ids)
-    print("minprice", min_price)
-    print("maxprice", max_price)
-    
-    # Construct the API request URL
+    # print("attributes", attribute_ids)
+    # print("occasions", occasion_ids)
+    # print("relations", relation_ids)
+    # print("minprice", min_price)
+    # print("maxprice", max_price)
+
+    # Construct the API request URL with multiple occasionId and relationshipId parameters
     api_url = (
         f'https://api.toandfrom.com/v3/recommendation/testing?isApplyFilter=true'
-        f'&occasionId={",".join(occasion_ids)}'
-        f'&relationshipId={",".join(relation_ids)}'
         f'&minPrice={min_price}'
         f'&maxPrice={max_price}'
         f'&attributeIds={",".join(attribute_ids)}'
     )
-    
+
+    # Append multiple occasionId and relationshipId parameters
+    for occasion_id in occasion_ids:
+        api_url += f'&occasionId={occasion_id}'
+    for relation_id in relation_ids:
+        api_url += f'&relationshipId={relation_id}'
+
+    # print("API URL", api_url)
+
     # Call the API and get the list of products
     headers = {
         'content-type': 'application/json',
         'revision': '2024-05-23'
     }
-    response = requests.post(api_url, headers=headers, timeout=10)
-    
+    response = requests.get(api_url, headers=headers, timeout=10)
+
     if response.status_code == 200:
         products = response.json()
         # Print the list of products
         print(json.dumps(products, indent=4))
     else:
         print(f"API request failed with status code {response.status_code}")
-        print(f"Response text: {response.text}")
-    
-    # Ask for a new query
-    query = input("User Query: ")
-
-print('Bye!')
+        print(f"API Response text: {response.text}")
