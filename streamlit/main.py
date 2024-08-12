d5596b748de793c6731fe6def98ebabbbee5d983
Prateikx <iampratik8@gmail.com>
2024-08-12 16:56:30 +0530

    setup deployments file

--- streamlit/main.py
+++ streamlit/main.py
@@ -4,14 +4,14 @@ import requests
 # Set the page configuration
 st.set_page_config(
     page_title="Kloudstax: To&From",
-    layout="wide"  # This sets the layout to be wide
+    layout="wide" 
 )
 
 # Streamlit app title
 st.title("Kloudstax: To&From")
 
 # User input for query
-user_query = st.text_input("Enter your query:")
+user_query = st.text_area("Enter your query:", height=100)
 
 # User input for Bearer Token
 bearer_token = st.text_input("Enter your Bearer Token:", type="password")
@@ -35,10 +35,10 @@ def fetch_product_details(product_id):
         if data.get("image") and len(data["image"]) > 0:
             return decodeURIComponent(data["image"][0]["link"])
         else:
-            return "https://via.placeholder.com/100"  # Fallback image URL
+            return "https://via.placeholder.com/100"
     except requests.RequestException as e:
         st.error(f"Error fetching product details for {product_id}: {e}")
-        return "https://via.placeholder.com/100"  # Fallback image URL
+        return "https://via.placeholder.com/100"
 
 # Function to decode URI component
 def decodeURIComponent(url):
@@ -73,7 +73,7 @@ if st.button("Send Request"):
                 with col:
                     product_id = product.get("id", "")
                     image_url = fetch_product_details(product_id)
-                    st.image(image_url, width=150, caption=f"Rank: {product.get('rank', 'No rank')}")
+                    st.image(image_url, width=200, caption=f"Rank: {product.get('rank', 'No rank')}")
                     st.write(f"Product ID: {product_id}")
                     product_url = product.get('url', 'No URL')
                     if product_url != 'No URL':
@@ -85,13 +85,13 @@ if st.button("Send Request"):
             # Display Debug Information with Images
             st.header("Debug Information")
             debug_info = data.get("debug", [])
-            cols = st.columns(4)  # Number of columns for layout
+            cols = st.columns(4)
 
             for idx, debug_item in enumerate(debug_info):
                 col = cols[idx % 4]
                 with col:
                     image_url = fetch_product_details(debug_item.get("id", ""))
-                    st.image(image_url, width=150, caption=debug_item.get("name", "No name provided"))
+                    st.image(image_url, width=200, caption=debug_item.get("name", "No name provided"))
                     st.write(f"Brand: {debug_item.get('brandName', 'No brand name')}")
                     st.write(f"Description: {debug_item.get('description', 'No description')}")
                     st.write(f"Price: {debug_item.get('price', 'No price')/100}")
